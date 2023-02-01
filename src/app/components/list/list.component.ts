ec5354f18cddb1221951d8bbc2f9667533ae90ff
mehdi makhlouf <mehdi.makhlouf@external.thalesgroup.com>
2023-02-01 19:40:15 +0100

    clean content

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Product } from 'src/app/common/product-model';
+import { products } from 'src/app/common/products';
 
 @Component({
     selector: 'app-list',
@@ -13,4 +16,23 @@ export class ListExampleComponent {
         { category: 'SOUTH AMERIKA', value: 0.09 },
         { category: 'AFRICA', value: 0.05 }
     ];
+    public gridData: Product[] = products;
+    public pageSize = 10;
+    public formGroup: undefined | FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {}
+
+    public createFormGroup = (args: any): FormGroup => {
+        const item = args.isNew ? new Product() : args.dataItem;
+
+        this.formGroup = this.formBuilder.group({
+            ProductID: item.ProductID,
+            ProductName: [item.ProductName, Validators.required],
+            UnitPrice: item.UnitPrice,
+            UnitsInStock: [item.UnitsInStock, Validators.compose([Validators.required, Validators.pattern('^[0-9]{1,3}')])],
+            Discontinued: item.Discontinued
+        });
+
+        return this.formGroup;
+    }
 }
